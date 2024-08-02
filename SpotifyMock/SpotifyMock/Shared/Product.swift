1e7877409a9a9fe39339669bb606595f5446dbe1
Oriol Angelet <uriaeri@gmail.com>
2024-08-02 18:30:42 +0200

    Updated Product & User model

--- SpotifyMock/SpotifyMock/Shared/Product.swift
+++ SpotifyMock/SpotifyMock/Shared/Product.swift
@@ -12,17 +12,48 @@ struct ProductArray: Codable {
     let total, skip, limit: Int
 }
 
-    // MARK: - Product
 struct Product: Codable, Identifiable {
     let id: Int
     let title, description: String
-    let price, discountPercentage, rating: Double
+    let price: Double // Double not Int
+    let discountPercentage, rating: Double
     let stock: Int
-    let tags: [String]
-    let brand: String?
-    let sku: String
-    let weight: Int
-    let minimumOrderQuantity: Int
-    let images: [String]
+    let brand: String? // <- Optional
+    let category: String
     let thumbnail: String
+    let images: [String]
+    
+    var firstImage: String {
+        images.first ?? Constants.randomImage
+    }
+    
+    var _brand: String {
+        brand ?? ""
+    }
+    
+    let recentlyAdded: Bool = {
+        return Int.random(in: 1...4) == 1
+    }()
+    
+    static var mock: Product {
+        Product(
+            id: 123,
+            title: "Example product title",
+            description: "This is some mock product description that goes here.",
+            price: 999,
+            discountPercentage: 15,
+            rating: 4.5,
+            stock: 50,
+            brand: "Apple",
+            category: "Electronic Devices",
+            thumbnail: Constants.randomImage,
+            images: [Constants.randomImage, Constants.randomImage, Constants.randomImage]
+        )
+    }
+}
+
+struct ProductRow: Identifiable {
+    let id = UUID().uuidString
+    let title: String
+    let products: [Product]
 }
