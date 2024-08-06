8b5e06059fa2dc021297feeebf1655950792913a
Oriol Angelet <uriaeri@gmail.com>
2024-08-06 19:20:31 +0200

    L6, pt3 - First iteration of header in SpotifyPlaylistView

--- SpotifyMock/SpotifyMock/Spotify/Core/SpotifyPlaylistView.swift
+++ SpotifyMock/SpotifyMock/Spotify/Core/SpotifyPlaylistView.swift
@@ -13,6 +13,8 @@ struct SpotifyPlaylistView: View {
     var user: User = .mock
     
     @State private var products: [Product] = []
+    @State private var showHeader: Bool = true
+    
     
     var body: some View {
         ZStack {
@@ -51,10 +53,33 @@ struct SpotifyPlaylistView: View {
                                 
                             }
                     }
-                    .padding(.horizontal, 16)
+                    .padding(.leading, 16)
                 }
             }
             .scrollIndicators(.hidden)
+            
+            ZStack {
+                Text(product.title)
+                    .font(.headline)
+                    .foregroundStyle(.spotifyWhite)
+                    .padding(.vertical, 20)
+                    .frame(maxWidth: .infinity)
+                    .background(.blue)
+                    .opacity(showHeader ? 1 : 0)
+                
+                Image(systemName: "chevron.left")
+                    .font(.title3)
+                    .padding(10)
+                    .background(showHeader ? .black.opacity(0.001) : .spotifyGray.opacity(0.7))
+                    .clipShape(Circle())
+                    .onTapGesture {
+                        
+                    }
+                    .padding(.leading, 16)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+            }
+            .foregroundStyle(.spotifyWhite)
+            .frame(maxHeight: .infinity, alignment: .top)
         }
         .task {
             await getData()
