f3726293d0a1faa0d4b9fffc84d3ed046b12e6e5
Oriol Angelet <uriaeri@gmail.com>
2024-08-06 18:58:01 +0200

    L6, pt1 - SongRowCell created

---
+++ SpotifyMock/SpotifyMock/Spotify/Components/SongRowCell.swift
@@ -0,0 +1,61 @@
+//
+//  SongRowCell.swift
+//  SpotifyMock
+//
+//  Created by Uri on 6/8/24.
+//
+
+import SwiftUI
+
+struct SongRowCell: View {
+    
+    var imageSize: CGFloat = 50
+    var imageName: String = Constants.randomImage
+    var title: String = "Some song name goes here"
+    var subtitle: String? = "Some artist name"
+    var onCellPressed: (() -> Void)? = nil
+    var onEllipsisPressed: (() -> Void)? = nil
+    
+    var body: some View {
+        HStack(spacing: 8) {
+            ImageLoaderView(urlString: imageName)
+                .frame(width: imageSize, height: imageSize)
+            
+            VStack(alignment: .leading, spacing: 4) {
+                Text(title)
+                    .font(.body)
+                    .fontWeight(.medium)
+                    .foregroundStyle(.spotifyWhite)
+                
+                if let subtitle {
+                    Text(subtitle)
+                        .font(.callout)
+                        .foregroundStyle(.spotifyLightGray)
+                }
+            }
+            .lineLimit(2)
+            .frame(maxWidth: .infinity, alignment: .leading)
+            
+            Image(systemName: "ellipsis")
+                .font(.subheadline)
+                .padding(16)    // helps clickability
+                .background(.black.opacity(0.001)) // helps clickability
+                .foregroundStyle(.spotifyWhite)
+                .onTapGesture {
+                    onEllipsisPressed?()
+                }
+        }
+    }
+}
+
+#Preview {
+    ZStack {
+        Color.black.ignoresSafeArea()
+        
+        SongRowCell()
+        SongRowCell()
+        SongRowCell()
+        SongRowCell()
+        SongRowCell()
+    }
+}
