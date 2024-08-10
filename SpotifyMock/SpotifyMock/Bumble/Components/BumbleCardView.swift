086d254bcc542d29a4f7261b10caa7b06e8cc7d1
Oriol Angelet <uriaeri@gmail.com>
2024-08-10 18:14:43 +0200

    Bumble L2, pt3 - HeaderCell extracted to a private var

--- SpotifyMock/SpotifyMock/Bumble/Components/BumbleCardView.swift
+++ SpotifyMock/SpotifyMock/Bumble/Components/BumbleCardView.swift
@@ -14,39 +14,12 @@ struct BumbleCardView: View {
     var body: some View {
         ScrollView(.vertical) {
             LazyVStack(spacing: 0) {
-                
-                ZStack(alignment: .bottomLeading) {
-                    ImageLoaderView(urlString: user.image)
-                    
-                    VStack(alignment: .leading, spacing: 8) {
-                        Text("\(user.firstName), \(user.age)")
-                            .font(.largeTitle)
-                            .fontWeight(.semibold)
-                        
-                        HStack(spacing: 4) {
-                            Image(systemName: "suitcase")
-                            Text(user.work)
-                        }
-                        HStack(spacing: 4) {
-                            Image(systemName: "graduationcap")
-                            Text(user.education)
-                        }
-                        
-                        BumbleHeartView()
-                            .onTapGesture {
-                                
-                            }
-                    }
-                    .padding(24)
-                    .font(.callout)
-                    .fontWeight(.medium)
-                    .foregroundStyle(.bumbleWhite)
-                    
-                }
-                .frame(height: 600)
+                headerCell
+                    .frame(height: 700)
             }
         }
         .scrollIndicators(.hidden)
+        .clipShape(.rect(cornerRadius: 32))
     }
 }
 
@@ -55,3 +28,50 @@ struct BumbleCardView: View {
         .padding(.vertical, 40)
         .padding(.horizontal, 16)
 }
+
+extension BumbleCardView {
+    
+    private var headerCell: some View {
+        ZStack(alignment: .bottomLeading) {
+            ImageLoaderView(urlString: user.image)
+            
+            VStack(alignment: .leading, spacing: 8) {
+                Text("\(user.firstName), \(user.age)")
+                    .font(.largeTitle)
+                    .fontWeight(.semibold)
+                
+                HStack(spacing: 4) {
+                    Image(systemName: "suitcase")
+                    Text(user.work)
+                }
+                HStack(spacing: 4) {
+                    Image(systemName: "graduationcap")
+                    Text(user.education)
+                }
+                
+                BumbleHeartView()
+                    .onTapGesture {
+                        
+                    }
+            }
+            .padding(24)
+            .font(.callout)
+            .fontWeight(.medium)
+            .foregroundStyle(.bumbleWhite)
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .background(
+                LinearGradient(
+                    colors: [
+                        .black.opacity(0),
+                        .black.opacity(0.6),
+                        .black.opacity(0.6)
+                    ],
+                    startPoint: .top,
+                    endPoint: .bottom
+                )
+            )
+        }
+    }
+}
+
+// black.opacity(0.6) twice to extend the opacity
