d75ba183735c7bfedf47ecc3c47ea9e4ec3ef410
Oriol Angelet <uriaeri@gmail.com>
2024-08-10 17:15:44 +0200

    Bumble L1, pt 2 - header finished

--- SpotifyMock/SpotifyMock/Bumble/Core/BumbleHomeView.swift
+++ SpotifyMock/SpotifyMock/Bumble/Core/BumbleHomeView.swift
@@ -13,45 +13,54 @@ struct BumbleHomeView: View {
             Color.bumbleWhite.ignoresSafeArea()
             
             VStack(spacing: 8) {
-                HStack(spacing: 0) {
-                    HStack(spacing: 0) {
-                        Image(systemName: "line.horizontal.3")
-                            .padding(8)
-                            .background(.black.opacity(0.001))
-                            .onTapGesture {
-                                
-                            }
-                        
-                        Image(systemName: "arrow.uturn.left")
-                            .padding(8)
-                            .background(.black.opacity(0.001))
-                            .onTapGesture {
-                                
-                            }
-                    }
-                    .background(.red)
-                    
-                    Text("bumble")
-                        .font(.title)
-                        .foregroundStyle(.bumbleYellow)
-                        .background(.blue)
-                    
-                    Image(systemName: "slider.horizontal.3")
-                        .padding(8)
-                        .background(.black.opacity(0.001))
-                        .onTapGesture {
-                            
-                        }
-                        .background(.red)
-                }
-                .font(.title2)
-                .fontWeight(.medium)
-                .foregroundStyle(.bumbleBlack)
+                header
+                Spacer()
             }
+            .padding(8)
         }
     }
 }
 
 #Preview {
     BumbleHomeView()
+}
+
+extension BumbleHomeView {
+    
+    private var header: some View {
+        HStack(spacing: 0) {
+            HStack(spacing: 0) {
+                Image(systemName: "line.horizontal.3")
+                    .padding(8)
+                    .background(.black.opacity(0.001))
+                    .onTapGesture {
+                        
+                    }
+                
+                Image(systemName: "arrow.uturn.left")
+                    .padding(8)
+                    .background(.black.opacity(0.001))
+                    .onTapGesture {
+                        
+                    }
+            }
+            .frame(maxWidth: .infinity, alignment: .leading)
+            
+            Text("bumble")
+                .font(.title)
+                .foregroundStyle(.bumbleYellow)
+                .frame(maxWidth: .infinity, alignment: .center)
+            
+            Image(systemName: "slider.horizontal.3")
+                .padding(8)
+                .background(.black.opacity(0.001))
+                .onTapGesture {
+                    
+                }
+                .frame(maxWidth: .infinity, alignment: .trailing)
+        }
+        .font(.title2)
+        .fontWeight(.medium)
+        .foregroundStyle(.bumbleBlack)
+    }
 }
