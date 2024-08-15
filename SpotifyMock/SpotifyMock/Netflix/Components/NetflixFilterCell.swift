3190c38ea14337eeea8101781695872f9e932c92
Oriol Angelet <uriaeri@gmail.com>
2024-08-15 17:51:53 +0200

    Netflix L1, pt3 - NetflixFilterCell created

---
+++ SpotifyMock/SpotifyMock/Netflix/Components/NetflixFilterCell.swift
@@ -0,0 +1,54 @@
+//
+//  NetflixFilterCell.swift
+//  SpotifyMock
+//
+//  Created by Uri on 15/8/24.
+//
+
+import SwiftUI
+
+struct NetflixFilterCell: View {
+    
+    var title: String = "Categories"
+    var isDropdown: Bool = true
+    var isSelected: Bool = false
+    
+    var body: some View {
+        HStack(spacing: 4) {
+            Text(title)
+            
+            if isDropdown {
+                Image(systemName: "chevron.down")
+            }
+        }
+        .padding(.horizontal, 16)
+        .padding(.vertical, 8)
+        .background(
+            ZStack {
+                if isSelected {
+                    Capsule(style: .circular)
+                        .fill(.netflixDarkGray)
+                }
+
+                Capsule(style: .circular)
+                    .stroke(lineWidth: 1)
+            }
+        )
+        .foregroundStyle(.netflixLightGray)
+    }
+}
+
+#Preview {
+    ZStack {
+        Color.black.ignoresSafeArea()
+        
+        VStack {
+            NetflixFilterCell()
+            NetflixFilterCell(isSelected: true)
+            NetflixFilterCell(isDropdown: false)
+        }
+        
+    }
+}
+
+// tip: set paddings based on multiples (4,8,16...)
