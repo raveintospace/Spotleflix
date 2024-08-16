8bef053b0e1a8b9485fd15ebde6271d56b1e2ddd
Oriol Angelet <uriaeri@gmail.com>
2024-08-16 18:06:12 +0200

    Netflix L2, pt1 - NetflixHeroCell created

---
+++ SpotifyMock/SpotifyMock/Netflix/Components/NetflixHeroCell.swift
@@ -0,0 +1,115 @@
+//
+//  NetflixHeroCell.swift
+//  SpotifyMock
+//
+//  Created by Uri on 16/8/24.
+//
+
+import SwiftUI
+import SwiftfulUI
+
+struct NetflixHeroCell: View {
+    
+    var imageName: String = Constants.randomImage
+    var isNetflixFilm: Bool = true
+    var title: String = "The Gentlemen"
+    var categories: [String] = ["Comedy", "Mafia", "Crime"]
+    var onBackgroundPressed: (() -> Void)? = nil
+    var onPlayPressed: (() -> Void)? = nil
+    var onMyListPressed: (() -> Void)? = nil
+    
+    var body: some View {
+        ZStack(alignment: .bottom) {
+            ImageLoaderView(urlString: imageName)
+            
+            VStack(spacing: 16) {
+                VStack(spacing: 0) {
+                    if isNetflixFilm {
+                        HStack(spacing: 8) {
+                            Text("N")
+                                .font(.largeTitle)
+                                .fontWeight(.black)
+                                .foregroundStyle(.netflixRed)
+                            
+                            Text("FILM")
+                                .font(.subheadline)
+                                .fontWeight(.semibold)
+                                .kerning(3)
+                                .foregroundStyle(.netflixWhite)
+                        }
+                    }
+                    
+                    Text(title)
+                        .font(.system(size: 40, weight: .medium, design: .serif))
+                }
+   
+                HStack(spacing: 8) {
+                    ForEach(categories, id: \.self) { category in
+                        Text(category)
+                            .font(.callout)
+                        
+                        if category != categories.last {
+                            Circle()
+                                .frame(width: 4, height: 4)
+                        }
+                    }
+                }
+                
+                HStack(spacing: 16) {
+                    HStack {
+                        Image(systemName: "play.fill")
+                        Text("Play")
+                    }
+                    .frame(maxWidth: .infinity)
+                    .padding(.vertical, 8)
+                    .foregroundStyle(.netflixDarkGray)
+                    .background(.netflixWhite)
+                    .clipShape(.rect(cornerRadius: 4))
+                    .asButton {
+                        onPlayPressed?()
+                    }
+                    HStack {
+                        Image(systemName: "plus")
+                        Text("My List")
+                    }
+                    .frame(maxWidth: .infinity)
+                    .padding(.vertical, 8)
+                    .foregroundStyle(.netflixWhite)
+                    .background(.netflixDarkGray)
+                    .clipShape(.rect(cornerRadius: 4))
+                    .asButton {
+                        onMyListPressed?()
+                    }
+                }
+                .font(.callout)
+                .fontWeight(.medium)
+            }
+            .padding(24)
+            .background(
+                LinearGradient(
+                    colors: [
+                        .netflixBlack.opacity(0),
+                        .netflixBlack.opacity(0.4),
+                        .netflixBlack.opacity(0.4),
+                        .netflixBlack.opacity(0.4)
+                    ],
+                    startPoint: .top,
+                    endPoint: .bottom
+                )
+            )
+        }
+        .foregroundStyle(.netflixWhite)
+        .clipShape(.rect(cornerRadius: 10))
+        .aspectRatio(0.8, contentMode: .fit)
+        .asButton(.tap) {
+            onBackgroundPressed?()
+        }
+    }
+}
+
+#Preview {
+    NetflixHeroCell()
+        .padding(40)
+}
+
+// Linear Gradient affects the whole image, because is set after padding(24)
