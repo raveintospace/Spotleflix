8b2704f348c76583666855f1581fa23f344a3a97
Oriol Angelet <uriaeri@gmail.com>
2024-08-18 18:10:19 +0200

    Netflix L5, pt2 - NetflixDetailHeaderView created and integrated in DetailView

--- SpotifyMock/SpotifyMock/Netflix/Core/NetflixDetailView.swift
+++ SpotifyMock/SpotifyMock/Netflix/Core/NetflixDetailView.swift
@@ -11,13 +11,29 @@ struct NetflixDetailView: View {
     
     var product: Product = .mock
     
+    @State private var progress: Double = 0.2
+    
     var body: some View {
         ZStack {
             Color.black.ignoresSafeArea()
             Color.netflixDarkGray.opacity(0.3).ignoresSafeArea()
             
             VStack(spacing: 0) {
+                NetflixDetailHeaderView(
+                    imageName: product.firstImage,
+                    progress: progress,
+                    onAirplayPressed: {
+                        
+                    },
+                    onXMarkPressed: {
+                        
+                    }
+                )
                 
+                ScrollView(.vertical) {
+                    
+                }
+                .scrollIndicators(.hidden)
             }
         }
     }
